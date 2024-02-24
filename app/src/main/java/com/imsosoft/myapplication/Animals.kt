6abb87af463d975d356ce1554ab12f0d6b9dccba
Muarem <s.muarem10@gmail.com>
2024-02-24 18:21:47 +0400

    created Dog, Cat, Animals class

---
+++ app/src/main/java/com/imsosoft/myapplication/Animals.kt
@@ -0,0 +1,9 @@
+package com.imsosoft.myapplication
+
+class Animals(cat: Cat, dog: Dog) {
+
+    fun food() {
+        println("animals ate food")
+    }
+
+}
---
+++ app/src/main/java/com/imsosoft/myapplication/Dog.kt
@@ -0,0 +1,4 @@
+package com.imsosoft.myapplication
+
+class Dog {
+}
